0cb7518f15a7797a9a8c819c9eb0863b677eaadc
Aibol Maxotov <aibol.maxotov@btsdigital.kz>
2019-12-02 19:28:59 +0600

    Explore Container widget

---
+++ lib/app_screens/home.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Container(
+      alignment: Alignment.center,
+      color: Colors.deepPurple,
+//		    width: 200.0,
+//		    height: 100.0,
+//		    margin: EdgeInsets.only(left: 35.0, top: 50.0),
+      child: Text(
+        "Flight",
+        textDirection: TextDirection.ltr,
+        style: TextStyle(
+            decoration: TextDecoration.none,
+            fontSize: 75.0,
+            fontFamily: 'Raleway',
+            fontWeight: FontWeight.w700,
+            color: Colors.white),
+      ),
+    ));
+  }
+}
