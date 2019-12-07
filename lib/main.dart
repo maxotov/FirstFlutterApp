739cd1da4683d5b9c453f83c42d3246bb205cf4e
Aibol Maxotov <aibol.maxotov@btsdigital.kz>
2019-12-07 21:56:54 +0600

    Create Simple Flutter App

--- lib/main.dart
+++ lib/main.dart
@@ -1,64 +1,209 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(title: "Stateful App Example", home: FavoriteCity()));
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    title: 'Simple Interest Calculator App',
+    home: SIForm(),
+    theme: ThemeData(
+        brightness: Brightness.dark,
+        primaryColor: Colors.indigo,
+        accentColor: Colors.indigoAccent),
+  ));
 }
 
-class FavoriteCity extends StatefulWidget {
+class SIForm extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
-    return _FavoriteCityState();
+    return _SIFormState();
   }
 }
 
-class _FavoriteCityState extends State<FavoriteCity> {
-  String nameCity = "";
-  var _currencies = ['Rupees', 'Dollars', 'Pounds', 'Others'];
-  var _currentItemSelected = 'Dollars';
+class _SIFormState extends State<SIForm> {
+  var _formKey = GlobalKey<FormState>();
+
+  var _currencies = ['Rupees', 'Dollars', 'Pounds'];
+  final double _minimumPadding = 5.0;
+
+  var _currentItemSelected = '';
+
+  @override
+  void initState() {
+    super.initState();
+    _currentItemSelected = _currencies[0];
+  }
+
+  TextEditingController principalController = TextEditingController();
+  TextEditingController roiController = TextEditingController();
+  TextEditingController termController = TextEditingController();
+
+  var displayResult = '';
 
   @override
   Widget build(BuildContext context) {
-    debugPrint("Favorite City widget is created");
+    TextStyle textStyle = Theme.of(context).textTheme.title;
 
     return Scaffold(
+//			resizeToAvoidBottomPadding: false,
       appBar: AppBar(
-        title: Text("Stateful App Example"),
+        title: Text('Simple Interest Calculator'),
       ),
-      body: Container(
-        margin: EdgeInsets.all(20.0),
-        child: Column(
-          children: <Widget>[
-            TextField(
-              onSubmitted: (String userInput) {
-                setState(() {
-                  debugPrint(
-                      "set State is called, this tells framwork to redraw the FavCity widget");
-                  nameCity = userInput;
-                });
-              },
-            ),
-            DropdownButton<String>(
-              items: _currencies.map((String dropDownStringItem) {
-                return DropdownMenuItem<String>(
-                  value: dropDownStringItem,
-                  child: Text(dropDownStringItem),
-                );
-              }).toList(),
-              onChanged: (String newValueSelected) {
-                // Your code to execute, when a menu item is selected from drop down
-                _onDropDownItemSelected(newValueSelected);
-              },
-              value: _currentItemSelected,
-            ),
-            Padding(
-                padding: EdgeInsets.all(30.0),
-                child: Text(
-                  "Your bext city is $nameCity",
-                  style: TextStyle(fontSize: 20.0),
-                ))
-          ],
-        ),
+
+      body: Form(
+        key: _formKey,
+        child: Padding(
+            padding: EdgeInsets.all(_minimumPadding * 2),
+            child: ListView(
+              children: <Widget>[
+                getImageAsset(),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      controller: principalController,
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter principal amount';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Principal',
+                          hintText: 'Enter Principal e.g. 12000',
+                          labelStyle: textStyle,
+                          errorStyle: TextStyle(
+                              color: Colors.yellowAccent, fontSize: 15.0),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      style: textStyle,
+                      controller: roiController,
+                      validator: (String value) {
+                        if (value.isEmpty) {
+                          return 'Please enter rate of interest';
+                        }
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Rate of Interest',
+                          hintText: 'In percent',
+                          labelStyle: textStyle,
+                          errorStyle: TextStyle(
+                              color: Colors.yellowAccent, fontSize: 15.0),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(5.0))),
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: _minimumPadding, bottom: _minimumPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                            child: TextFormField(
+                          keyboardType: TextInputType.number,
+                          style: textStyle,
+                          controller: termController,
+                          validator: (String value) {
+                            if (value.isEmpty) {
+                              return 'Please enter time';
+                            }
+                          },
+                          decoration: InputDecoration(
+                              labelText: 'Term',
+                              hintText: 'Time in years',
+                              labelStyle: textStyle,
+                              errorStyle: TextStyle(
+                                  color: Colors.yellowAccent, fontSize: 15.0),
+                              border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(5.0))),
+                        )),
+                        Container(
+                          width: _minimumPadding * 5,
+                        ),
+                        Expanded(
+                            child: DropdownButton<String>(
+                          items: _currencies.map((String value) {
+                            return DropdownMenuItem<String>(
+                              value: value,
+                              child: Text(value),
+                            );
+                          }).toList(),
+                          value: _currentItemSelected,
+                          onChanged: (String newValueSelected) {
+                            // Your code to execute, when a menu item is selected from dropdown
+                            _onDropDownItemSelected(newValueSelected);
+                          },
+                        ))
+                      ],
+                    )),
+                Padding(
+                    padding: EdgeInsets.only(
+                        bottom: _minimumPadding, top: _minimumPadding),
+                    child: Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: RaisedButton(
+                            color: Theme.of(context).accentColor,
+                            textColor: Theme.of(context).primaryColorDark,
+                            child: Text(
+                              'Calculate',
+                              textScaleFactor: 1.5,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                if (_formKey.currentState.validate()) {
+                                  this.displayResult = _calculateTotalReturns();
+                                }
+                              });
+                            },
+                          ),
+                        ),
+                        Expanded(
+                          child: RaisedButton(
+                            color: Theme.of(context).primaryColorDark,
+                            textColor: Theme.of(context).primaryColorLight,
+                            child: Text(
+                              'Reset',
+                              textScaleFactor: 1.5,
+                            ),
+                            onPressed: () {
+                              setState(() {
+                                _reset();
+                              });
+                            },
+                          ),
+                        ),
+                      ],
+                    )),
+                Padding(
+                  padding: EdgeInsets.all(_minimumPadding * 2),
+                  child: Text(
+                    this.displayResult,
+                    style: textStyle,
+                  ),
+                )
+              ],
+            )),
       ),
+    );
+  }
+
+  Widget getImageAsset() {
+    AssetImage assetImage = AssetImage('images/money.png');
+    Image image = Image(
+      image: assetImage,
+      width: 125.0,
+      height: 125.0,
+    );
+
+    return Container(
+      child: image,
+      margin: EdgeInsets.all(_minimumPadding * 10),
     );
   }
 
@@ -66,5 +211,25 @@ class _FavoriteCityState extends State<FavoriteCity> {
     setState(() {
       this._currentItemSelected = newValueSelected;
     });
+  }
+
+  String _calculateTotalReturns() {
+    double principal = double.parse(principalController.text);
+    double roi = double.parse(roiController.text);
+    double term = double.parse(termController.text);
+
+    double totalAmountPayable = principal + (principal * roi * term) / 100;
+
+    String result =
+        'After $term years, your investment will be worth $totalAmountPayable $_currentItemSelected';
+    return result;
+  }
+
+  void _reset() {
+    principalController.text = '';
+    roiController.text = '';
+    termController.text = '';
+    displayResult = '';
+    _currentItemSelected = _currencies[0];
   }
 }
